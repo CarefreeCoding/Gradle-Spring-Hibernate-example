127f46118ec4de2bdd699720a8fbf8d1a28a0d47
CarefreeCoding <carefree.coding@gmail.com>
2014-11-20 09:08:26 -0500

    Simple index page
    Moved everything to make simple index page. Very silly.

--- src/main/java/example/controllers/IndexController.java
+++ src/main/java/example/controllers/IndexController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/welcome")
-public class HelloController
+@RequestMapping("/")
+public class IndexController
 {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model)
 	{
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "hello";
+		model.addAttribute("message", "Hello World!!!");
+		return "indexPage";
 	}
 }
