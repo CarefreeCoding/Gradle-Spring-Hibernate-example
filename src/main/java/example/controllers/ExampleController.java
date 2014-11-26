9178cebd4c06fbea35a38995b1faf1c5a21478bf
CarefreeCoding <carefree.coding@gmail.com>
2014-11-26 18:26:32 -0500

    All works gg
    This is done and works. Gg no more

--- src/main/java/example/controllers/ExampleController.java
+++ src/main/java/example/controllers/ExampleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/example")
 public class ExampleController
@@ -25,7 +27,11 @@ public class ExampleController
 	public String get()
 	{
 		System.out.println("Called get");
-		return new Gson().toJson(new ExampleObject());
+		List<ExampleObject> list = database.get();
+		System.out.println("Loaded |" + list + "|");
+		String response = new Gson().toJson(database.get());
+		System.out.println("Returning |" + response + "|");
+		return response;
 	}
 
 	@RequestMapping(value = "/get/{id}",
@@ -34,8 +40,12 @@ public class ExampleController
 	@ResponseBody
 	public String get(@PathVariable String id)
 	{
-		System.out.println("Received |" + id + "|");
-		return new Gson().toJson(new ExampleObject());
+		System.out.println("Getting |" + id + "|");
+		ExampleObject object = database.get(id);
+		System.out.println("Loaded |" + object + "|");
+		String response = new Gson().toJson(object);
+		System.out.println("Returning |" + response + "|");
+		return response;
 	}
 
 	@RequestMapping(value = "/add/{name}",
@@ -44,9 +54,14 @@ public class ExampleController
 	@ResponseBody
 	public String add(@PathVariable String name)
 	{
+		System.out.println("Received |" + name + "|");
 		ExampleObject object = new ExampleObject();
 		object.setName(name);
-		System.out.println(object);
-		return new Gson().toJson(new ExampleObject());
+		System.out.println("Adding |" + object + "|");
+		database.add(object);
+		System.out.println("Added |" + object + "|");
+		String response = new Gson().toJson(object);
+		System.out.println("Returning |" + response + "|");
+		return response;
 	}
 }
